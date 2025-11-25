175c91863e1abbf505276f4bbc955e3e9740c749
viqwwa <viktoriamalyhina597@gmail.com>
2025-11-25 19:28:24 +0300

    добавление 6 задачи

---
+++ task 6/Heap.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_6
+{
+    public enum HeapType
+    {
+        MinHeap, MaxHeap
+    }
+    public class Heap<T>
+    {
+        public T[] array { get; set; }
+        public int size { get; set; }
+        private int capacity;
+        private HeapType type;
+        private IComparer<T> comparer;
+
+        public Heap(T[] array, HeapType heapType, IComparer<T> comparer)
+        {
+            size = 0;
+            capacity = array.Length;
+            this.array = new T[capacity];
+            type = heapType;
+            this.comparer = comparer ?? Comparer<T>.Default;
+
+            for (int i = 0; i < capacity; i++)
+            {
+                this.array[i] = array[i];
+                size++;
+                SiftUp(i);
+            }
+        }
+
+        public T Peek()
+        {
+            IsEmpty();
+            return array[0];
+        }
+
+        public void Insert(T element)
+        {
+            if (size == capacity)
+            {
+                if (capacity < 64)
+                {
+                    capacity += 2;
+                }
+                else
+                {
+                    capacity += capacity / 2;
+                }
+            }
+            
+            T[] newArray = new T[capacity];
+            for (int i = 0; i < size; i++)
+            {
+                newArray[i] = array[i];
+            }
+            array = newArray;
+
+            array[size++] = element;
+            SiftUp(size - 1);
+        }
+
+        public T Pop()
+        {
+            IsEmpty();
+            T top = array[0];
+            T lastValue = array[size - 1];
+            array[0] = lastValue;
+            size--;
+            SiftDown(0);
+            return top;
+        }
+
+        public void ChangeKey(int index, T newValue)
+        {
+            if (index > size - 1)
+            {
+                throw new ArgumentOutOfRangeException("Такого элемента нет в куче");
+            }
+
+            T oldValue = array[index];
+            array[index] = newValue;
+
+            if (ToSwap(oldValue, newValue))
+            {
+                SiftDown(index);
+            }
+            else
+            {
+                SiftUp(index);
+            }
+        }
+
+        static public Heap<T> MergeHeaps(Heap<T> heap1, Heap<T> heap2)
+        {
+            if (heap1.type != heap2.type)
+            {
+                throw new InvalidOperationException("Нельзя произвести слияние куч разного типа");
+            }
+
+            T[] mergedArray = new T[heap1.size + heap2.size];
+
+            for (int i = 0; i < heap1.size; i++)
+            {
+                mergedArray[i] = heap1.array[i];
+            }
+
+            for (int i = 0; i < heap2.size; i++)
+            {
+                mergedArray[i + heap1.size] = heap2.array[i];
+            }
+
+            Heap<T> newHeap = new Heap<T>(mergedArray, heap1.type, heap1.comparer);
+            return newHeap;
+        }
+
+        public void SiftUp(int index)
+        {
+            while (index > 0)
+            {
+                int parentIndex = (index - 1) / 2;
+
+                if (ToSwap(array[index], array[parentIndex]))
+                {
+                    (array[index], array[parentIndex]) = (array[parentIndex], array[index]);
+                    index = parentIndex;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        public void SiftDown(int index)
+        {
+            while (index * 2 + 1 < size)
+            {
+                int leftIndex = index * 2 + 1;
+                int rightIndex = index * 2 + 2;
+                int minChildIndex = leftIndex;
+
+                if (rightIndex < size && ToSwap(array[rightIndex], array[leftIndex]))
+                {
+                    minChildIndex = rightIndex;
+                }
+
+                if (ToSwap(array[index], array[minChildIndex]))
+                {
+                    break;
+                }
+                else
+                {
+                    (array[index], array[minChildIndex]) = (array[minChildIndex], array[index]);
+                    index = minChildIndex;
+                }
+            }
+        }
+
+        private bool ToSwap(T element1, T element2)
+        {
+            int result = comparer.Compare(element1, element2);
+            return type == HeapType.MinHeap ? result < 0 : result > 0;
+        }
+
+        public void IsEmpty()
+        {
+            if (size == 0)
+            {
+                throw new InvalidOperationException("Куча пуста");
+            }
+        }
+    }
+}
---
+++ task 6/Program.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_6
+{
+    internal class Program
+    {
+        class MyPriorityQueue<T>
+        {
+            private Heap<T> queue;
+            private int size;
+            private IComparer<T> comparer;
+
+            public MyPriorityQueue()
+            {
+                T[] array = new T[11];
+                size = 0;
+                comparer = Comparer<T>.Default;
+                queue = new Heap<T>(array, HeapType.MinHeap, comparer);
+            }
+
+            public MyPriorityQueue(T[] array)
+            {
+                size = array.Length;
+                comparer = Comparer<T>.Default;
+                queue = new Heap<T>(array, HeapType.MinHeap, comparer);
+            }
+
+            public MyPriorityQueue(int initialCapacity)
+            {
+                if (initialCapacity < 0)
+                {
+                    throw new ArgumentException("Ёмкость должна быть положительной");
+                }
+
+                T[] array = new T[initialCapacity];
+                size = 0;
+                comparer = Comparer<T>.Default;
+                queue = new Heap<T>(array, HeapType.MinHeap, comparer);
+            }
+
+            public MyPriorityQueue(int initialCapacity, IComparer<T> comparer)
+            {
+                if (initialCapacity < 0)
+                {
+                    throw new ArgumentException("Ёмкость должна быть положительной");
+                }
+
+                size = 0;
+                this.comparer = comparer;
+                T[] array = new T[initialCapacity];
+                queue = new Heap<T>(array, HeapType.MinHeap, comparer);
+            }
+
+            public MyPriorityQueue(MyPriorityQueue<T> q)
+            {
+                size = q.size;
+                comparer = q.comparer;
+                T[] newArray = new T[q.queue.array.Length];
+                Array.Copy(q.queue.array, newArray, q.size);
+                queue = new Heap<T>(newArray, HeapType.MinHeap, comparer);
+            }
+
+            public void Add(T item)
+            {
+                queue.Insert(item);
+                size++;
+            }
+
+            public void AddAll(T[] array)
+            {
+                foreach (T item in array)
+                {
+                    queue.Insert(item);
+                    size++;
+                }
+            }
+
+            public void Clear()
+            {
+                size = 0;
+                queue.size = 0;
+            }
+
+            public bool Contains(T itemSearch)
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    if (comparer.Compare(itemSearch, queue.array[i]) == 0) return true;
+                }
+                return false;
+            }
+
+            public bool ContainsAll(T[] array)
+            {
+                foreach (T item in array)
+                {
+                    if (!Contains(item)) return false;
+                }
+                return true;
+            }
+
+            public bool IsEmpty()
+            {
+                return size == 0;
+            }
+
+            public void Remove(T itemRemove)
+            {
+                if (IsEmpty())
+                {
+                    throw new InvalidOperationException("Очередь пуста");
+                }
+                for (int i = 0; i < size; i++)
+                {
+                    if (comparer.Compare(itemRemove, queue.array[i]) == 0)
+                    {
+                        T removedItem = queue.array[i];
+                        queue.array[i] = queue.array[size - 1];
+                        size--;
+                        queue.size--;
+                        queue.SiftDown(i);
+                        queue.SiftUp(i);
+                        break;
+                    }
+                }
+            }
+
+            public void RemoveAll(T[] array)
+            {
+                foreach (T item in array)
+                {
+                    Remove(item);
+                }
+            }
+
+            public void RetainAll(T[] array)
+            {
+                bool flag = false;
+                foreach (T item in queue.array)
+                {
+                    foreach (T itemRetain in array)
+                    {
+                        if (comparer.Compare(itemRetain, item) == 0) flag = true;
+                    }
+                    if (!flag)
+                    {
+                        Remove(item);
+                    }
+                }
+            }
+
+            public int Size() => size;
+
+            public T[] ToArray()
+            {
+                return queue.array;
+            }
+
+            public T[] ToArray(T[] array)
+            {
+                if (array == null)
+                {
+                    return ToArray();
+                }
+                if (array.Length < size)
+                {
+                    throw new ArgumentException("Длина массива недостаточна");
+                }
+                for (int i = 0; i < size; i++)
+                {
+                    array[i] = queue.array[i];
+                }
+                return array;
+            }
+
+            public T Element()
+            {
+                return queue.Peek();
+            }
+
+            public Object Peek()
+            {
+                try
+                {
+                    return queue.Peek();
+                }
+                catch (Exception ex)
+                {
+                    return null;
+                }
+            }
+
+            public Object Poll()
+            {
+                try
+                {
+                    size--;
+                    return queue.Pop();
+                }
+                catch (Exception ex)
+                {
+                    return null;
+                }
+            }
+
+            public bool Offer(T item)
+            {
+                if (!typeof(T).IsValueType && item == null)
+                {
+                    return false;
+                }
+                Add(item);
+                return true;
+            }
+
+            public void Print()
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    Console.Write(queue.array[i]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                MyPriorityQueue<int> queue = new MyPriorityQueue<int>();
+                queue.Remove(2);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
