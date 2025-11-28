95750cb4c2d5a34ab9d47a7e8fc7ca18cb3d6707
viqwwa <viktoriamalyhina597@gmail.com>
2025-11-28 20:03:07 +0300

    добавление 7 задачи

---
+++ task 7/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace task_7
+{
+    internal class Program
+    {
+        public class Bid
+        {
+            public int priority;
+            public int id;
+            public int iteration;
+
+            public Bid()
+            {
+                priority = 0;
+                id = 0;
+                iteration = 0;
+            }
+
+            public Bid(int priority, int id, int iteration)
+            {
+                this.priority = priority;
+                this.id = id;
+                this.iteration = iteration;
+            }
+
+            public void Log(string operation)
+            {
+                try
+                {
+                    using (StreamWriter sr = new StreamWriter("log.txt", true))
+                    {
+                        sr.WriteLine($"{operation} {id} {priority} {iteration}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            public void Print()
+            {
+                Console.WriteLine($"{id} {priority} {iteration}");
+            }
+        }
+
+        public static IComparer<Bid> BidComparer =>
+            Comparer<Bid>.Create((x, y) =>
+            {
+                if (x == null && y == null) return 0;
+                if (x == null) return -1;
+                if (y == null) return 1;
+                int priorityCompare = x.priority.CompareTo(y.priority);
+                return priorityCompare != 0 ? priorityCompare : x.id.CompareTo(y.id);
+            });
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            MyPriorityQueue<Bid> queue = new MyPriorityQueue<Bid>(n, BidComparer);
+            Random random = new Random();
+            int index = 1;
+            Bid lastDeleted = new Bid();
+
+            for (int i = 0; i < n; i++)
+            {
+                int count = random.Next(1, 11);
+
+                for (int j = 0; j < count; j++)
+                {
+                    int priority = random.Next(1, 6);
+                    Bid bid = new Bid(priority, index++, i);
+                    queue.Add(bid);
+                    bid.Log("ADD");
+                }
+
+                lastDeleted = queue.Poll();
+                lastDeleted.Log("REMOVE");
+            }
+
+            while (!queue.IsEmpty())
+            {
+                lastDeleted = queue.Poll();
+                lastDeleted.Log("REMOVE");
+            }
+
+            Console.WriteLine("Заявка, которая ожидала максимальное время");
+            lastDeleted.Print();
+        }
+    }
+}
---
+++ task 7/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("task 7")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("task 7")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("aa103b5d-0504-4c6d-a0fa-a0c862b34db0")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
