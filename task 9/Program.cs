c60c48ce6f57d3aa3aafb1db714ce31976398010
viqwwa <viktoriamalyhina597@gmail.com>
2025-12-05 00:30:28 +0300

    добавление 9 задачи

---
+++ task 9/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace task_9
+{
+    internal class Program
+    {
+        static MyArrayList<string> ReadData()
+        {
+            MyArrayList<string> tags = new MyArrayList<string>();
+
+            try
+            {
+                using (StreamReader sr = new StreamReader("input.txt"))
+                {
+                    string line;
+                    string pattern = @"<\/?[a-zA-Z][a-zA-Z0-9]*>";
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        MatchCollection matches = Regex.Matches(line, pattern);
+
+                        foreach (Match match in matches)
+                        {
+                            tags.Add(match.Value);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return tags;
+        }
+
+        static string NormalizeTeg(string tag)
+        {
+            string content = tag.Trim('<', '>').TrimStart('/');
+            return content.ToLowerInvariant();
+        }
+
+        static void DeleteDublicates(MyArrayList<string> tags)
+        {
+            MyArrayList<string> uniqueTags = new MyArrayList<string>();
+
+            for (int i = 0; i < tags.Size(); i++)
+            {
+                string normTag = NormalizeTeg(tags.Get(i));
+
+                if (!uniqueTags.Contains(normTag))
+                {
+                    uniqueTags.Add(normTag);
+                }
+                else
+                {
+                    tags.Remove(i);
+                    i--;
+                }
+            }
+        }
+
+        static void Print(MyArrayList<string> tags)
+        {
+            for (int i = 0; i < tags.Size(); i++)
+            {
+                Console.Write(tags.Get(i) + " ");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            MyArrayList<string> tags = ReadData();
+            DeleteDublicates(tags);
+            Print(tags);
+        }
+    }
+}
---
+++ task 9/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("task 9")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("task 9")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("0e802d42-0218-4c20-b082-5ac8cd1a4e67")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
